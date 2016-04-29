b314446542ad2bd27486b83712a2a8a3902fcdcd
SMP <sam@mageplaza.com>
2016-04-29 23:12:44 +0700

    Auto update

--- assets/js/init.js
+++ assets/js/init.js
@@ -32,19 +32,20 @@
 
     // Floating-Fixed table of contents
     setTimeout(function() {
-      var tocWrapperHeight = 260; // Max height of ads.
+      var tocWrapperHeight = 500; // Max height of ads.
       var tocHeight = $('.toc-wrapper .table-of-contents').length ? $('.toc-wrapper .table-of-contents').height() : 0;
       var socialHeight = 95; // Height of unloaded social media in footer.
       var footerOffset = $('footer').first().length ? $('footer').first().offset().top : 0;
       var bottomOffset = footerOffset - socialHeight - tocHeight - tocWrapperHeight;
 
-      if ($('nav').length) {
-        $('.toc-wrapper').pushpin({
-          top: $('nav').height(),
-          bottom: bottomOffset
-        });
-      }
-      else if ($('#index-banner').length) {
+      // if ($('nav').length) {
+      //   $('.toc-wrapper').pushpin({
+      //     top: $('nav').height(),
+      //     bottom: bottomOffset
+      //   });
+      // }
+      
+      if ($('#index-banner').length) {
         $('.toc-wrapper').pushpin({
           top: $('#index-banner').height(),
           bottom: bottomOffset
